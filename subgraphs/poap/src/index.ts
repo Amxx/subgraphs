298f72f782e4b0f78c4921a6716e4b3144e5b188
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-03-05 20:42:07 +0100

    enable eventHandlers

--- subgraphs/poap/src/index.ts
+++ subgraphs/poap/src/index.ts
@@ -27,76 +27,69 @@ import {
 } from '@amxx/graphprotocol-utils'
 
 export function handleTransfer(event: TransferEvent): void {
-	let token = new Token(event.params.tokenId.toString());
-	let from  = new Account(event.params.from.toHex());
-	let to    = new Account(event.params.to.toHex());
+	let token        = new Token(event.params.tokenId.toString())
+	let from         = new Account(event.params.from.toHex())
+	let to           = new Account(event.params.to.toHex())
+	token.identifier = event.params.tokenId
+	token.owner      = to.id
+	token.save()
+	from.save()
+	to.save()
 
-	token.owner = to.id;
-
-	token.save();
-	from.save();
-	to.save();
-
-	let ev = new Transfer(events.id(event));
+	let ev         = new Transfer(events.id(event))
 	ev.transaction = transactions.log(event).id
-	ev.timestamp   = event.block.timestamp;
-	ev.token       = token.id;
-	ev.from        = from.id;
-	ev.to          = to.id;
-	ev.save();
+	ev.timestamp   = event.block.timestamp
+	ev.token       = token.id
+	ev.from        = from.id
+	ev.to          = to.id
+	ev.save()
 }
 
 export function handleApproval(event: ApprovalEvent): void {
-	let token    = new Token(event.params.tokenId.toString());
-	let owner    = new Account(event.params.owner.toHex());
-	let approved = new Account(event.params.approved.toHex());
+	let token      = new Token(event.params.tokenId.toString())
+	let owner      = new Account(event.params.owner.toHex())
+	let approved   = new Account(event.params.approved.toHex())
+	token.approval = approved.id
+	token.save()
+	owner.save()
+	approved.save()
 
-	token.approval = approved.id;
-
-	token.save();
-	owner.save();
-	approved.save();
-
-	let ev = new Approval(events.id(event));
+	let ev         = new Approval(events.id(event))
 	ev.transaction = transactions.log(event).id
-	ev.timestamp   = event.block.timestamp;
-	ev.token       = token.id;
-	ev.owner       = owner.id;
-	ev.approved    = approved.id;
-	ev.save();
+	ev.timestamp   = event.block.timestamp
+	ev.token       = token.id
+	ev.owner       = owner.id
+	ev.approved    = approved.id
+	ev.save()
 }
 
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-	let owner      = new Account(event.params.owner.toHex());
-	let operator   = new Account(event.params.operator.toHex());
-	let delegation = new OperatorDelegation(owner.id.concat('-').concat(operator.id));
+	let owner           = new Account(event.params.owner.toHex())
+	let operator        = new Account(event.params.operator.toHex())
+	let delegation      = new OperatorDelegation(owner.id.concat('-').concat(operator.id))
+	delegation.owner    = owner.id
+	delegation.operator = operator.id
+	delegation.approved = event.params.approved
+	owner.save()
+	operator.save()
+	delegation.save()
 
-	delegation.owner    = owner.id;
-	delegation.operator = operator.id;
-	delegation.approved = event.params.approved;
-
-	owner.save();
-	operator.save();
-	delegation.save();
-
-	let ev = new ApprovalForAll(events.id(event));
+	let ev         = new ApprovalForAll(events.id(event))
 	ev.transaction = transactions.log(event).id
-	ev.timestamp   = event.block.timestamp;
-	ev.delegation  = delegation.id;
-	ev.owner       = owner.id;
-	ev.operator    = operator.id;
-	ev.approved    = event.params.approved;
-	ev.save();
+	ev.timestamp   = event.block.timestamp
+	ev.delegation  = delegation.id
+	ev.owner       = owner.id
+	ev.operator    = operator.id
+	ev.approved    = event.params.approved
+	ev.save()
 }
 
 export function handleEventToken(event: EventTokenEvent): void {
-	let poapevent = new PoapEvent(event.params.eventId.toString());
-	let token     = new Token(event.params.tokenId.toString());
-
-	token.event = poapevent.id;
-
-	poapevent.save();
-	token.save();
+	let poapevent = new PoapEvent(event.params.eventId.toString())
+	let token     = new Token(event.params.tokenId.toString())
+	token.event   = poapevent.id
+	poapevent.save()
+	token.save()
 }
 
 export function handleAdminAdded(event: AdminAddedEvent): void {
