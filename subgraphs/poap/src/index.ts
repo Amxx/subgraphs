12a15a5968878ce3fa933a2568e244ae174d6671
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-03-05 18:09:02 +0100

    add approval & approvalForAll to POAP

--- subgraphs/poap/src/index.ts
+++ subgraphs/poap/src/index.ts
@@ -1,5 +1,4 @@
 import {
-	POAP,
 	AdminAdded         as AdminAddedEvent,
 	AdminRemoved       as AdminRemovedEvent,
 	Approval           as ApprovalEvent,
@@ -15,45 +14,17 @@ import {
 import {
 	Account,
 	Token,
+	OperatorDelegation,
 	PoapEvent,
 	Transfer,
+	Approval,
+	ApprovalForAll,
 } from '../generated/schema'
 
 import {
 	events,
 	transactions,
 } from '@amxx/graphprotocol-utils'
-
-export function handleAdminAdded(event: AdminAddedEvent): void {
-}
-
-export function handleAdminRemoved(event: AdminRemovedEvent): void {
-}
-
-export function handleApproval(event: ApprovalEvent): void {
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-}
-
-export function handleEventMinterAdded(event: EventMinterAddedEvent): void {
-}
-
-export function handleEventMinterRemoved(event: EventMinterRemovedEvent): void {
-}
-
-export function handleEventToken(event: EventTokenEvent): void {
-	let poapevent = new PoapEvent(event.params.eventId.toString());
-	let token     = new Token(event.params.tokenId.toString());
-
-	token.event = poapevent.id;
-
-	poapevent.save();
-	token.save();
-}
-
-export function handlePaused(event: PausedEvent): void {
-}
 
 export function handleTransfer(event: TransferEvent): void {
 	let token = new Token(event.params.tokenId.toString());
@@ -73,6 +44,74 @@ export function handleTransfer(event: TransferEvent): void {
 	ev.from        = from.id;
 	ev.to          = to.id;
 	ev.save();
+}
+
+export function handleApproval(event: ApprovalEvent): void {
+	let token    = new Token(event.params.tokenId.toString());
+	let owner    = new Account(event.params.owner.toHex());
+	let approved = new Account(event.params.approved.toHex());
+
+	token.approval = approved.id;
+
+	token.save();
+	owner.save();
+	approved.save();
+
+	let ev = new Approval(events.id(event));
+	ev.transaction = transactions.log(event).id
+	ev.timestamp   = event.block.timestamp;
+	ev.token       = token.id;
+	ev.owner       = owner.id;
+	ev.approved    = approved.id;
+	ev.save();
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+	let owner      = new Account(event.params.owner.toHex());
+	let operator   = new Account(event.params.operator.toHex());
+	let delegation = new OperatorDelegation(owner.id.concat('-').concat(operator.id));
+
+	delegation.owner    = owner.id;
+	delegation.operator = operator.id;
+	delegation.approved = event.params.approved;
+
+	owner.save();
+	operator.save();
+	delegation.save();
+
+	let ev = new ApprovalForAll(events.id(event));
+	ev.transaction = transactions.log(event).id
+	ev.timestamp   = event.block.timestamp;
+	ev.delegation  = delegation.id;
+	ev.owner       = owner.id;
+	ev.operator    = operator.id;
+	ev.approved    = event.params.approved;
+	ev.save();
+}
+
+export function handleEventToken(event: EventTokenEvent): void {
+	let poapevent = new PoapEvent(event.params.eventId.toString());
+	let token     = new Token(event.params.tokenId.toString());
+
+	token.event = poapevent.id;
+
+	poapevent.save();
+	token.save();
+}
+
+export function handleAdminAdded(event: AdminAddedEvent): void {
+}
+
+export function handleAdminRemoved(event: AdminRemovedEvent): void {
+}
+
+export function handleEventMinterAdded(event: EventMinterAddedEvent): void {
+}
+
+export function handleEventMinterRemoved(event: EventMinterRemovedEvent): void {
+}
+
+export function handlePaused(event: PausedEvent): void {
 }
 
 export function handleUnpaused(event: UnpausedEvent): void {
