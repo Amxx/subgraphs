e151fc649f71c5854ba8dae963313d5c448480cd
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-06-18 14:53:13 +0200

    add recovery support

---
+++ subgraphs/argent/contracts/IRecoveryManager.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+import "./IBaseModule.sol";
+import "./IRelayerModule.sol";
+
+interface IRecoveryManager is IBaseModule, IRelayerModule {
+    event RecoveryExecuted(address indexed wallet, address indexed _recovery, uint64 executeAfter);
+    event RecoveryFinalized(address indexed wallet, address indexed _recovery);
+    event RecoveryCanceled(address indexed wallet, address indexed _recovery);
+    event OwnershipTransfered(address indexed wallet, address indexed _newOwner);
+
+    function recoveryPeriod() external view returns (uint256);
+    function lockPeriod() external view returns (uint256);
+    function securityPeriod() external view returns (uint256);
+    function securityWindow() external view returns (uint256);
+    function guardianStorage() external view returns (address);
+    function getRecovery(address _wallet) external view returns(address _address, uint64 _executeAfter, uint32 _guardianCount);
+    function getRequiredSignatures(address _wallet, bytes calldata _data) external view returns (uint256);
+
+    function executeRecovery(address _wallet, address _recovery) external;
+    function finalizeRecovery(address _wallet) external;
+    function cancelRecovery(address _wallet) external;
+    function transferOwnership(address _wallet, address _newOwner) external;
+}
---
+++ subgraphs/argent/src/datasources/RecoveryManager.ts
@@ -0,0 +1,78 @@
+import {
+	store,
+} from "@graphprotocol/graph-ts"
+
+import {
+  Recovery,
+  RecoveryExecuted,
+  RecoveryFinalized,
+  RecoveryCanceled,
+} from "../../generated/schema"
+
+import {
+  OwnershipTransfered as OwnershipTransferedEvent,
+  RecoveryCanceled    as RecoveryCanceledEvent,
+  RecoveryExecuted    as RecoveryExecutedEvent,
+  RecoveryFinalized   as RecoveryFinalizedEvent,
+} from "../../generated/RecoveryManager/RecoveryManager"
+
+import {
+	events,
+	transactions,
+} from '@amxx/graphprotocol-utils'
+
+import { fetchAccount  } from '../fetch/account'
+import { fetchWallet  } from '../fetch/wallet'
+
+export function handleOwnershipTransfered(event: OwnershipTransferedEvent): void {
+}
+
+export function handleRecoveryExecuted(event: RecoveryExecutedEvent): void {
+	let wallet   = fetchWallet(event.params.wallet)
+	let newOwner = fetchAccount(event.params._recovery)
+	let id       = wallet.id.concat('/').concat(newOwner.id)
+
+	let recovery          = new Recovery(id)
+	recovery.wallet       = wallet.id
+	recovery.newOwner     = newOwner.id
+	recovery.executeAfter = event.params.executeAfter
+	recovery.save()
+
+	let ev          = new RecoveryExecuted(events.id(event))
+	ev.transaction  = transactions.log(event).id
+	ev.timestamp    = event.block.timestamp
+	ev.wallet       = wallet.id
+	ev.newOwner     = newOwner.id
+	ev.executeAfter = event.params.executeAfter
+	ev.save()
+}
+
+export function handleRecoveryFinalized(event: RecoveryFinalizedEvent): void {
+	let wallet   = fetchWallet(event.params.wallet)
+	let newOwner = fetchAccount(event.params._recovery)
+	let id       = wallet.id.concat('/').concat(newOwner.id)
+
+	store.remove("Recovery", id)
+
+	let ev         = new RecoveryFinalized(events.id(event))
+	ev.transaction = transactions.log(event).id
+	ev.timestamp   = event.block.timestamp
+	ev.wallet      = wallet.id
+	ev.newOwner    = newOwner.id
+	ev.save()
+}
+
+export function handleRecoveryCanceled(event: RecoveryCanceledEvent): void {
+	let wallet   = fetchWallet(event.params.wallet)
+	let newOwner = fetchAccount(event.params._recovery)
+	let id       = wallet.id.concat('/').concat(newOwner.id)
+
+	store.remove("Recovery", id)
+
+	let ev         = new RecoveryCanceled(events.id(event))
+	ev.transaction = transactions.log(event).id
+	ev.timestamp   = event.block.timestamp
+	ev.wallet      = wallet.id
+	ev.newOwner    = newOwner.id
+	ev.save()
+}
