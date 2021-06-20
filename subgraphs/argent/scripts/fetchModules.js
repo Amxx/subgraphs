4e6a61e9d77f7379b15caeb1efff909202f32960
Hadrien Croubois <hadrien.croubois@gmail.com>
2021-06-20 14:47:29 +0200

    fix fetchModule script

--- subgraphs/argent/scripts/fetchModules.js
+++ subgraphs/argent/scripts/fetchModules.js
@@ -1,5 +1,5 @@
 const axios = require('axios')
-const MODULES = require('./src/modules.json')
+const MODULES = require('../config/argent-mainnet.json')
 
 function run(url, query, variables) {
   return new Promise((resolve, reject) => {
@@ -14,15 +14,16 @@ function run(url, query, variables) {
 
 async function main () {
   const { modules } = await run(
-    'https://api.thegraph.com/subgraphs/id/QmTX1ePDcBRzJQGYEy8qsedgpNpNz3ybQqHw2Gs8Mf7xAz',
+    'https://api.thegraph.com/subgraphs/name/amxx/argent',
+    // 'https://api.thegraph.com/subgraphs/id/QmTX1ePDcBRzJQGYEy8qsedgpNpNz3ybQqHw2Gs8Mf7xAz',
     '{ modules { id }}'
   );
 
   console.log(`Modules:`);
   console.log(`- ${modules.length} found`);
-  console.log(`- ${Object.values(MODULES).length} registered`);
+  console.log(`- ${MODULES.datasources.length} registered`);
 
-  const missing = modules.filter(({ id }) => !MODULES[id])
+  const missing = modules.filter(({ id }) => !MODULES.datasources.find(({ address }) => id == address.toLowerCase()));
   if (missing.some(Boolean)) {
     for (const { id } of missing) {
       console.log(`Missing module: ${id}`);
@@ -30,7 +31,6 @@ async function main () {
   } else {
     console.log('All good.')
   }
-
 }
 
 main()
